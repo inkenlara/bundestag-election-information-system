293f6f0a4b83cea784d3c4bd659d8043942e8add
zauberer1 <adi.karamehic@hotmail.com>
2022-11-23 17:27:27 +0100

    Q3 base

---
+++ src/wahlkreisubersicht.js
@@ -0,0 +1,68 @@
+var value = 10;
+google.charts.load('current', {'packages':['table']});
+// google.charts.setOnLoadCallback(drawTable);
+
+var menu = document.getElementById("wahlkreis_choice");
+menu.addEventListener("change", set_value);
+
+function set_value(event) {
+    value = menu.value;
+    google.charts.setOnLoadCallback(drawTable);
+    console.log(menu.value)
+}
+
+// The global variable value contains the Wahlkreis id, which can be used to filter the data
+function drawTable() {
+    let data = new google.visualization.DataTable();
+    data.addColumn('number', 'WahlBeteiligung');
+    data.addRows([
+        [ parseInt(value)] // in value is Wahlkreis ID
+    ]);
+    var table = new google.visualization.Table(document.getElementById('Wahlbeteiligung'));
+    table.draw(data, {showRowNumber: false, width: '100%', height: '100%'});
+
+
+    data = new google.visualization.DataTable();
+    data.addColumn('string', 'DirektKandidat VorName');
+    data.addColumn('string', 'DirektKandidat NachName');
+    data.addRows([
+        ['w1', 'p1'],
+        ['w2', 'p3'],
+        ['w3', 'p4'],
+        ['w4', 'p2']
+    ]);
+    var table = new google.visualization.Table(document.getElementById('Direktkandidaten_table'));
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+
+
+    data = new google.visualization.DataTable();
+    data.addColumn('string', 'Partei');
+    data.addColumn('number', 'Anzahl Stimmen');
+    data.addColumn('number', 'Prozent Stimmen')
+    data.addRows([
+        ['w1', 10, parseInt(value)],
+        ['w2', 12, 2],
+        ['w3', 3, 3],
+        ['w4', 4, 4]
+    ]);
+    var table = new google.visualization.Table(document.getElementById('pro_partei'));
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+
+    // TODO figure out how to structure
+    data = new google.visualization.DataTable();
+    data.addColumn('string', 'TODO');
+    data.addColumn('string', 'TODO');
+    data.addColumn('number', 'TODO')
+    data.addRows([
+        ['w1', 'p1', parseInt(value)],
+        ['w2', 'p3', 2],
+        ['w3', 'p4', 3],
+        ['w4', 'p2', 4]
+    ]);
+    var table = new google.visualization.Table(document.getElementById('entwicklung'));
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+
+
+  }
+
+
