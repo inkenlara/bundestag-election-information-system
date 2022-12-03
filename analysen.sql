2e08492cd63453aea5f3c27e37b4b5a4033dea84
Inken Grüner <inkengruner@MacBook-Air-von-Inken.local>
2022-12-04 00:06:35 +0100

    Q3, Q6

--- analysen.sql
+++ analysen.sql
@@ -4,6 +4,9 @@ SELECT p.KurzBezeichnung, s.sitze FROM sitzverteilungbundestag s, partei p
 WHERE s.partei = p.parteiid
 
 
+
+
+
 --Q2: Mitglieder Bundestag 2021
 
 --Teil fuer Direktmandate
@@ -69,7 +72,61 @@ and k.wahljahr = 2021
 and p.parteiid = k.partei
 
 
---Q3: Wahlkreisuebersicht
+
+
+
+
+--Q3: Wahlkreisuebersicht 2021
+
+--wahlbeteiligung
+select wahlkreis, (1.00*anzahlwahlende)/anzahlwahlberechtigte as wahlbeteiligung, wahljahr
+from wahlkreisaggretation
+and wahljahr = 2021
+    
+--gewaehlte direktkandidaten
+erststimmensieger as (
+  select we.wahlkreis, we.wahljahr, we.parteikurz as erststimmensieger, we.wahljahr
+  from wahlkreisprozenterst we
+  where we.wahljahr = 2021
+  and not exists                        
+          (select *                    
+          from wahlkreisprozenterst we2            
+          where we.wahlkreis = we2.wahlkreis  
+          and we2.wahljahr = 2021
+          and we2.prozenterststimmen > we.prozenterststimmen    
+          )
+)
+select k.kandidatid, k.firstname, k.lastname, e.erststimmensieger, k.wahljahr
+from erststimmensieger e, direktkandidaten dk, kandidaten k, partei p
+where e.erststimmensieger = p.KurzBezeichnung
+and p.parteiid = k.partei
+and k.kandidatid = dk.kandidatid
+and dk.wahlkreis = e.wahlkreis
+and k.wahljahr = 2021
+
+-- prozentuale und absolute Anzahl an Stimmen pro Partei + entwicklung im vergleich zum vorjahr
+with vorjahr as (
+    select w.wahlkreis, pz.parteikurz, w.anzahlstimmen, pz.prozentzweitstimmen
+    from wahlkreisprozentzweit pz, wahlkreiszweitstimmenaggregation w, partei p
+    where pz.wahljahr = 2017
+    and w.wahljahr = 2017
+    and pz.wahlkreis = w.wahlkreis
+    and w.partei = p.parteiid
+    and p.KurzBezeichnung = pz.parteikurz
+)
+select w.wahlkreis, pz.parteikurz, w.anzahlstimmen, pz.prozentzweitstimmen, w.anzahlstimmen-v.anzahlstimmen as stimmendifferenz, pz.prozentzweitstimmen-v.prozentzweitstimmen as prozentdifferenz
+from wahlkreisprozentzweit pz, wahlkreiszweitstimmenaggregation w, partei p, vorjahr v
+where pz.wahljahr = 2021
+and w.wahljahr = 2021
+and pz.wahlkreis = w.wahlkreis
+and w.partei = p.parteiid
+and p.KurzBezeichnung = pz.parteikurz
+and w.wahlkreis = v.wahlkreis
+and p.KurzBezeichnung = v.parteikurz
+
+
+
+
 
 --Q4: Wahlkreissieger 2021
 
@@ -106,5 +163,74 @@ with erststimmensieger as (
 --Q5: Ueberhangsmandate
 
 --Q6: Knappste Sieger
+-- filtere sieger
+with sieger as(
+    select *
+    from wahlkreisprozenterst w1
+    where w1.wahljahr = 2021
+    and not exists (select * from wahlkreisprozenterst w2 
+                    where w2.wahljahr = 2021 
+                    and w1.wahlkreis = w2.wahlkreis 
+                    and w2.prozenterststimmen > w1.prozenterststimmen)
+),
+-- filtere zweite
+wahlkreisprozenterst_ohne_sieger as(
+    select * from wahlkreisprozenterst
+    EXCEPT
+    select * from sieger    
+),
+zweite_sieger as(
+    select *
+    from wahlkreisprozenterst_ohne_sieger w1
+    where 
+    w1.wahljahr = 2021
+    and not exists (select * from wahlkreisprozenterst_ohne_sieger w2 
+                    where w2.wahljahr = 2021 
+                    and w1.wahlkreis = w2.wahlkreis 
+                    and w2.prozenterststimmen > w1.prozenterststimmen)    
+), 
+--bilde differenz zwischen stimmen
+differenz as (
+  select s.wahljahr, s.wahlkreis, s.parteikurz, s.prozenterststimmen-zs.prozenterststimmen as differenz, ROW_NUMBER() OVER(PARTITION BY s.parteikurz ORDER BY s.prozenterststimmen-zs.prozenterststimmen ASC) AS row_number
+  from sieger s, zweite_sieger zs
+  where s.wahlkreis = zs.wahlkreis
+)
+-- waehle die 10 knappsten abstaende
+select *
+from differenz
+where row_number <= 10
+
+-- Q6: wahlkreise, in der jede partei am knappsten verloren hat
+
+-- filtere sieger
+with sieger as(
+    select *
+    from wahlkreisprozenterst w1
+    where w1.wahljahr = 2021
+    and not exists (select * from wahlkreisprozenterst w2 
+                    where w2.wahljahr = 2021 
+                    and w1.wahlkreis = w2.wahlkreis 
+                    and w2.prozenterststimmen > w1.prozenterststimmen)
+),
+-- differenz zwischen gewinner und parteien
+differenz as(
+    select w.wahljahr, w.wahlkreis, w.parteikurz, s.prozenterststimmen-w.prozenterststimmen as differenz
+    from wahlkreisprozenterst w, sieger s
+    where w.wahljahr = 2021
+    and s.wahljahr = 2021
+    and w.parteikurz not in (select parteikurz from sieger) 
+    and w.wahlkreis = s.wahlkreis
+)
+-- suche fuer jede partei wahlkreis mit kleinster differenz
+select d1.wahljahr, d1.parteikurz, d1.wahlkreis ,d1.differenz
+from differenz d1
+where not exists (
+    select *
+    from differenz d2
+    where d1.parteikurz = d2.parteikurz
+    and d2.differenz < d1.differenz
+     )
+
+
 
 --Q7: Wahlkreisuebersicht (Einzelstimmen)
