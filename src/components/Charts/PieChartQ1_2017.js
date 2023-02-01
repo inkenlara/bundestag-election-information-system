41101c9ff9d2b3b194c636290b4c1aa1ba39a60a
zauberer1 <adi.karamehic@hotmail.com>
2023-02-01 14:50:08 +0100

    2017 sitze UI

---
+++ src/components/Charts/PieChartQ1_2017.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const PieChartq12017 = () => {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch('http://localhost:8000/query1_chart2017')
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  var options = {
+    maintainAspectRatio: false,
+    scales: {},
+    legend: {
+      labels: {
+        fontSize: 25,
+      },
+    },
+  };
+
+  var vals = {
+    labels: Object.keys(data),
+    datasets: [
+      {
+        // csu, cdu, linke, fdp, spd, grune, afd, ssw
+        label: 'Anzahl Sitze',
+        data: Object.values(data),
+        backgroundColor: [
+          'rgba(0, 0, 153, 0.2)',
+          'rgba(0, 0, 0, 0.2)',
+          'rgba(255, 0, 153, 0.2)',
+          'rgba(238, 255, 0, 0.2)',
+          'rgba(222, 78, 97, 0.2)',
+          'rgba(30, 252, 10, 0.2)',
+          'rgba(0, 153, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(0, 0, 153, 1)',
+          'rgba(0, 0, 0, 1)',
+          'rgba(255, 0, 153, 1)',
+          'rgba(238, 255, 0, 1)',
+          'rgba(222, 78, 97, 1)',
+          'rgba(30, 252, 10, 1)',
+          'rgba(0, 153, 255, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Pie data={vals} height={400} options={options} />
+    </div>
+  );
+};
+
+export default PieChartq12017;
--- src/pages/sitzverteilung.jsx
+++ src/pages/sitzverteilung.jsx
@@ -1,23 +1,63 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import PieChartq1 from "../components/Charts/PieChartQ1";
+import PieChartq1 from '../components/Charts/PieChartQ1';
+import PieChartq12017 from '../components/Charts/PieChartQ1_2017';
 
 class Sitzverteilung extends Component {
   state = {
     data: {},
-    url: "http://localhost:8000/query1_chart",
+    url: 'http://localhost:8000/query1_chart',
+    is2021: true,
   };
+
+  handleDisplay() {
+    var chec = document.getElementsByName('year-mit');
+    if (chec[0].checked) {
+      this.setState({ is2021: true });
+    } else {
+      this.setState({ is2021: false });
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <h2 /*onLoad={this.handleRequestNew}*/>Sitzverteilung</h2>
         {/*this.handleRequestNew()*/}
         {/*console.log(this.state.data)*/}
-        <div>
-          <PieChartq1 /*data={this.state.data}*/ /* options={ } */ />
-        </div>
-        <table onLoad={this.handleTable} id="tableSitze"></table>
-        {this.renderTable()}
+        <form>
+          <div>
+            <input
+              type="radio"
+              name="year-mit"
+              id="y2021"
+              value="2021"
+              defaultChecked
+            />
+            <label htmlFor="y2021">2021</label>
+
+            <input type="radio" name="year-mit" id="y2017" value="2017" />
+            <label htmlFor="y2017">2017</label>
+          </div>
+        </form>
+
+        <button
+          type="text"
+          className="choose-year"
+          onClick={() => {
+            this.handleDisplay();
+          }}
+        >
+          Choose
+        </button>
+        <div>{this.state.is2021 ? <PieChartq1 /> : <PieChartq12017 />}</div>
+        <table
+          onLoad={
+            this.state.is2021 ? this.renderTable() : this.renderTable2017()
+          }
+          id="tableSitze"
+        ></table>
+        {this.state.is2021 ? this.renderTable() : this.renderTable2017()}
       </React.Fragment>
     );
   }
@@ -39,23 +79,43 @@ class Sitzverteilung extends Component {
   }; */
 
   handleRequest() {
-    fetch("http://localhost:8000/query1_table")
+    fetch('http://localhost:8000/query1_table')
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
         // const newContent = document.createElement("table");
-        var tag_id = document.getElementById("tableSitze");
-        tag_id.innerHTML = data["data"];
-        return data["data"];
+        var tag_id = document.getElementById('tableSitze');
+        tag_id.innerHTML = data['data'];
+        return data['data'];
       })
       .catch(function (err) {
-        console.log("Fetch Error :-S", err);
+        console.log('Fetch Error :-S', err);
       });
   }
 
   renderTable() {
     return this.handleRequest();
+  }
+
+  handleRequest2017() {
+    fetch('http://localhost:8000/query1_table2017')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // const newContent = document.createElement("table");
+        var tag_id = document.getElementById('tableSitze');
+        tag_id.innerHTML = data['data'];
+        return data['data'];
+      })
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err);
+      });
+  }
+
+  renderTable2017() {
+    return this.handleRequest2017();
   }
 }
 
